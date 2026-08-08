6b5c5fc9611ad02b83b22bf8b9e5b2455bb4f1f4
你的名字 <>
2026-08-08 13:21:04 +0800

    初始化：Spring Boot项目 + 目录结构

---
+++ src/test/java/io/github/nihaoljx/flowchart/FlowchartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nihaoljx.flowchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowchartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
